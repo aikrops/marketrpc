4eabfc2464b8f144a117c8e8594dd0553c11cb29
aikrops <aikrops@tradingx.io>
2024-11-07 18:53:10 +0800

    更新时间的异常处理

--- marketrpc/rpcUtils.py
+++ marketrpc/rpcUtils.py
@@ -31,6 +31,8 @@ def datetime_to_millis(date_str: str) -> int:
     
     date_format = "%Y-%m-%d %H:%M:%S"
     try:
+        if len(date_str) == 10:
+            date_str = date_str + " 00:00:00"
         date_obj = datetime.strptime(date_str, date_format)
         timestamp_sec = date_obj.timestamp()
         timestamp_millisec = int(timestamp_sec * 1000)
@@ -58,16 +60,16 @@ def calculate_percentage(start_time, end_time, curr_time):
 
 # 查询市场K线数据。
 def market_kline(
-    exchange: str,
     account_type: str,
     symbol: str,
     kline_interval_second: int,
-    start_time: int,
-    end_time: int,
+    start_time: any,
+    end_time: any,
     start_id: int = 0,
     end_id: int = 0,
     type: str = "KLINE",
     limit: int = 10000,
+    exchange: str = "binance",
     is_asc: bool = True,
     is_gzip: bool = False
 ):
@@ -75,7 +77,7 @@ def market_kline(
     查询市场K线数据(倒序输出)
 
     :param type: 数据类型(default:KLINE)
-    :param exchange: 交易所名称
+    :param exchange: 交易所名称(default:binance)
     :param account_type: 账户类型
     :param symbol: 交易对
     :param kline_interval_second: K线时间间隔(1秒,1分钟,1小时)
@@ -102,18 +104,37 @@ def market_kline(
     :return StartId: 开始ID
     :return EndId: 结束ID
     """
-    # if not type:
-    #     raise ValueError("Type cannot be empty.")
+
     if not exchange:
-        raise ValueError("Exchange cannot be empty.")
+        raise ValueError("Exchange Exception Error.")
     if not account_type:
         raise ValueError("Account type cannot be empty.")
     if not symbol:
         raise ValueError("Symbol cannot be empty.")
+    if isinstance(start_time, str):
+        start_time = datetime_to_millis(start_time)
+    elif isinstance(start_time, int):
+        if len(str(start_time)) == 10:
+            start_time = int(start_time) * 1000
+        elif len(str(start_time)) == 13:
+            start_time = int(start_time)
+        else:
+            raise ValueError("Start time must be in seconds or milliseconds.")
+    if isinstance(end_time, str):
+        end_time = datetime_to_millis(end_time)
+    elif isinstance(end_time, int):
+        if len(str(end_time)) == 10:
+            end_time = int(end_time) * 1000
+        elif len(str(end_time)) == 13:
+            end_time = int(end_time)
+        else:
+            raise ValueError("End time must be in seconds or milliseconds.")
     if start_time > end_time:
         raise ValueError("Start time must be less than or equal to end time.")
     if kline_interval_second <= 0:
         raise ValueError("Kline interval second must be greater than 0.")
+    elif kline_interval_second not in [1, 60, 300, 600, 900, 1800]:
+        raise ValueError("Kline interval second must be 1, 60, 300, 600, 900, or 1800.")
     if not 1 <= limit <= 10000:
         raise ValueError("Limit must be between 1 and 10000.")
     
